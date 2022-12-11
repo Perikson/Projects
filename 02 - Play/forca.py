2d06469a465f615c4c714c9a61debdb2c425c246
Perikson <99338709+Perikson@users.noreply.github.com>
2022-12-11 17:51:10 -0300

    Update forca.py

--- 02 - Play/forca.py
+++ 02 - Play/forca.py
@@ -1,9 +1,67 @@
 import commun_functions
 
+
 def jogar():
     commun_functions.intro(' Bem vindo ao jogo da Forca! ')
 
-    print("Fim do jogo")
+    print('''\nTente descobrir qual é a palavra oculta antes de esgotar todas as suas tentativas.
+    
+    Escolha o nível de dificuldade:
+    
+    01 (Fácil) -  7 tentativas.
+    02 (Médio) -  5 tentativas.
+    03 (Dificil) - 3 tentativas)''')
 
-if(__name__ == "__main__"):
+    palavra_secreta = 'banana'.lower()
+    print(f'\nPalavra secreta: ', end=' ')
+
+    ps = ['*' for n in range(len(palavra_secreta))]
+    for l in ps:
+        print(l, end=' ')
+    print('\n')
+
+    tent = 0
+    while True:
+
+        nivel = int(input('Qual nível você quer tentar: '))
+        if nivel == 1:
+            tent = 7
+            break
+        elif nivel == 2:
+            tent = 5
+            break
+        elif nivel == 3:
+            tent = 3
+            break
+        else:
+            print('Digite uma opção válida.\n')
+
+    while True:
+
+        print(f'VocÊ possui {tent} tentativas.\n')
+        chute = input('Qual letra: ').strip().lower()
+
+        if chute in palavra_secreta:
+            for i, letra in enumerate(palavra_secreta):
+                if chute == letra:
+                    ps[i] = letra
+        else:
+            tent -= 1
+
+        print('\nA palavra secreta é: ')
+        for l in ps:
+            print(l, end=' ')
+        print()
+
+        if tent == 0:
+            print('Você perdeu.')
+            print("\nFim do jogo")
+            break
+
+        comparador = [n for n in palavra_secreta]
+        if ps == comparador:
+            print('Parabéns, Você venceu.')
+            break
+
+if (__name__ == "__main__"):
     jogar()
